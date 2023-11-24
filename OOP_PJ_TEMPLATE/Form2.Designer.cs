99f9a404845863226a5b52a47f1c6b502b467c12
cinnamonica02 <96633571+cinnamonica02@users.noreply.github.com>
2023-11-24 19:42:26 +0000

    Add files via upload

---
+++ OOP_PJ_TEMPLATE/Form2.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace $safeprojectname$
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add_student_title = new System.Windows.Forms.Label();
+            this.Name_label = new System.Windows.Forms.Label();
+            this.last_name_Label = new System.Windows.Forms.Label();
+            this.father_Name_Label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.date_of_birth_Label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.nationality_Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // add_student_title
+            // 
+            this.add_student_title.AutoSize = true;
+            this.add_student_title.Location = new System.Drawing.Point(216, 24);
+            this.add_student_title.Name = "add_student_title";
+            this.add_student_title.Size = new System.Drawing.Size(100, 20);
+            this.add_student_title.TabIndex = 0;
+            this.add_student_title.Text = "Add Student";
+            // 
+            // Name_label
+            // 
+            this.Name_label.AutoSize = true;
+            this.Name_label.Location = new System.Drawing.Point(40, 66);
+            this.Name_label.Name = "Name_label";
+            this.Name_label.Size = new System.Drawing.Size(44, 16);
+            this.Name_label.TabIndex = 1;
+            this.Name_label.Text = "Name";
+            // 
+            // last_name_Label
+            // 
+            this.last_name_Label.AutoSize = true;
+            this.last_name_Label.Location = new System.Drawing.Point(40, 107);
+            this.last_name_Label.Name = "last_name_Label";
+            this.last_name_Label.Size = new System.Drawing.Size(90, 20);
+            this.last_name_Label.TabIndex = 2;
+            this.last_name_Label.Text = "Last Name";
+            // 
+            // father_Name_Label
+            // 
+            this.father_Name_Label.AutoSize = true;
+            this.father_Name_Label.Location = new System.Drawing.Point(40, 152);
+            this.father_Name_Label.Name = "father_Name_Label";
+            this.father_Name_Label.Size = new System.Drawing.Size(106, 20);
+            this.father_Name_Label.TabIndex = 3;
+            this.father_Name_Label.Text = "Father Name";
+            this.father_Name_Label.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 4;
+            // 
+            // date_of_birth_Label
+            // 
+            this.date_of_birth_Label.AutoSize = true;
+            this.date_of_birth_Label.Location = new System.Drawing.Point(40, 203);
+            this.date_of_birth_Label.Name = "date_of_birth_Label";
+            this.date_of_birth_Label.Size = new System.Drawing.Size(99, 20);
+            this.date_of_birth_Label.TabIndex = 5;
+            this.date_of_birth_Label.Text = "Date of Birth";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(174, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(174, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(174, 257);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(134, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 50);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // nationality_Label
+            // 
+            this.nationality_Label.AutoSize = true;
+            this.nationality_Label.Location = new System.Drawing.Point(40, 263);
+            this.nationality_Label.Name = "nationality_Label";
+            this.nationality_Label.Size = new System.Drawing.Size(88, 20);
+            this.nationality_Label.TabIndex = 13;
+            this.nationality_Label.Text = "Nationality";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(376, 360);
+            this.Controls.Add(this.nationality_Label);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.date_of_birth_Label);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.father_Name_Label);
+            this.Controls.Add(this.last_name_Label);
+            this.Controls.Add(this.Name_label);
+            this.Controls.Add(this.add_student_title);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label add_student_title;
+        private System.Windows.Forms.Label Name_label;
+        private System.Windows.Forms.Label last_name_Label;
+        private System.Windows.Forms.Label father_Name_Label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label date_of_birth_Label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label nationality_Label;
+    }
+}
